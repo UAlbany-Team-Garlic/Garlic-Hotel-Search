79f374f6ba4038ea14deb7939a0a01c7d03ff412
James-Oswald <jamesoswald111@gmail.com>
2020-09-08 21:42:23 -0400

    added api and database templates

---
+++ source/server/API.js
@@ -0,0 +1,9 @@
+"use strict"
+
+/*
+    This file is for functions that connect the Hotel Info API to main backend, server.js
+*/
+
+function getInfo(data){
+    return //returned API Data
+}
---
+++ source/server/database.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/*
+    This file is for functions that connect the SQL database to main backend, server.js
+*/
+
+function newUser(data){
+    return //success?
+}
+
+function deleteUser(data){
+    return //success?
+}
+
+
+/*
+    Waiting on learning how we're storing ratings before implmenting ratings prototypes
+*/
+
+//function newRating
